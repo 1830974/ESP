3cd480256393273438725aa102af9895c31f09f9
1830974 <1830974@etu.cchic.ca>
2025-03-02 18:30:39 -0500

    Added NIP screen

---
+++ Paiement_1830974/Paiement_1830974/ViewModels/NIPVM.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Configuration;
+using Paiement_1830974.Resources;
+
+namespace Paiement_1830974.ViewModels
+{
+    public partial class NIPVM : ObservableObject
+    {
+        private readonly string _apiKey;
+        private readonly INavigationService _navigationService;
+        [ObservableProperty]
+        private int _nip;
+
+        public NIPVM(IConfiguration configuration, INavigationService navigationService)
+        {
+            _apiKey = configuration["ApiKey"];
+            _navigationService = navigationService;
+        }
+
+        partial void OnNipChanged(int value)
+        {
+            Console.WriteLine($"NIP changed to: {value}");
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        [RelayCommand]
+        private void Confirm()
+        {
+            if (CanConfirm())
+                Debug.WriteLine($"Confirmation button clicked with NIP: {Nip}");
+            else
+                Debug.WriteLine("Bad NIP Length");
+        }
+
+        private bool CanConfirm()
+        {
+            return Nip >= 1000 && Nip <= 9999;
+        }
+    }
+}
--- Paiement_1830974/Paiement_1830974/Views/NIP.xaml.cs
+++ Paiement_1830974/Paiement_1830974/Views/NIP.xaml.cs
@@ -12,17 +12,28 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Paiement_1830974.Resources;
+using Paiement_1830974.ViewModels;
 
 namespace Paiement_1830974.Views
 {
     /// <summary>
     /// Logique d'interaction pour NIP.xaml
     /// </summary>
-    public partial class PIN : Page
+    public partial class NIP : Page
     {
-        public PIN()
+        public NIP(NIPVM nipVM)
         {
             InitializeComponent();
+            this.DataContext = nipVM;
+        }
+
+        private void PinPasswordBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox passwordBox)
+            {
+                PasswordHelper.AttachBoundNip(passwordBox);
+            }
         }
     }
 }
